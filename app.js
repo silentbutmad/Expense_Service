cb56f22cc6fd55f52296c2221c7eb4abf61e5d6f
silentbutmad <mohitmahajan489@gmail.com>
2026-02-20 16:37:20 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+//import expenseRoutes from './routes/expenseRoutes'
+
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use("/",(req,res)=>{
+    res.send("HI");
+})
+//app.use('/api/expenses', expenseRoutes)
+
+app.listen(5000, () => {
+  console.log("Expense Service running on port 5000")
+})
